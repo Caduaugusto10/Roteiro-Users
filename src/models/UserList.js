c7665fdec541188582751d2441980fcf896727f5
Carlos Eduardo Ferraz Augusto <cadu.augusto10@gmail.com>
2025-02-18 10:59:33 -0300

    feat: user script

---
+++ src/models/UserList.js
@@ -0,0 +1,33 @@
+class UserList {
+    constructor() {
+        this.users = [];
+    }
+
+    addUser(user) {
+        this.users.push(user);
+    }
+
+    getAllUsers() {
+        return this.users;
+    }
+
+    getUserById(id) {
+        const user = this.users.find(user => user.id == id);
+        if(!user) {
+            throw new Error('User não encontrado');
+        }
+        return user;
+    }
+
+    updateUser(id, updatedData) {
+        const user = this.getUserById(id);
+        Object.assign(user, updatedData);
+        return user;
+    }
+
+    deleteUser(id) {
+        this.users = this.users.filter(user => user.id != id);
+    }
+}
+
+module.exports = UserList;
